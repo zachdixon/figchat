ca2b6229f7cd6cc1681e739ed77f93f9a9e426f6
Wesley Robinson <wes@Wesleys-MBP.attlocal.net>
2020-10-17 16:48:08 -0500

    Add chat screen navigation

---
+++ screens/Chat.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { StyleSheet, Image, TextInput } from "react-native";
+import { Entypo } from "@expo/vector-icons";
+
+import { Text, View, ScrollView, Button } from "../components/Themed";
+
+export default function Chats({ navigation }) {
+  return (
+    <View>
+    	<Text>Chat here!</Text>
+    </View>
+  )
+}
