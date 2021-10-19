40101bc2dcb00776298221760ffb87ea5a9c3655
Zachary Dixon <rissk13@gmail.com>
2021-10-19 00:10:57 -0500

    added basic drawing

--- screens/Fig.tsx
+++ screens/Fig.tsx
@@ -1,11 +1,19 @@
 import * as React from "react";
+import ExpoDraw from "expo-draw";
 
 import { Text, View } from "../components/Themed";
 
-export default function Chat() {
+export default function Fig() {
   return (
-    <View>
-    	<Text>Chat here!</Text>
+    <View style={{flex: 1}}>
+    	<ExpoDraw
+        strokes={[]}
+        containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
+        color={'#000000'}
+        strokeWidth={4}
+        enabled={true}
+        onChangeStrokes={(strokes) => console.log(strokes)}
+      />
     </View>
   )
 }
